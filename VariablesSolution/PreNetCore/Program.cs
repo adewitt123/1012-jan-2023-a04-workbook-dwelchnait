1c7b7f7553c53b8c3c140320dcd9a3dab58673ac
Don Welch <dwelch@nait.ca>
2023-01-17 14:50:30 -0700

    First console program investigating variables

---
+++ VariablesSolution/PreNetCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PreNetCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ VariablesSolution/Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//line comments start with a set of slashes
+/*
+ * comment blocks start with a slash-asterisk and terminate with
+ *     a asterisk-slash
+ */
+string myName = "Don";
+int myAge = 42;
+Console.WriteLine($"Hello {myName} to the World! I see you are only {myAge} years old");
