14681ed8917d3971387ccc81ef32134670166c53
Don Welch <dwelch@nait.ca>
2023-01-20 14:50:51 -0700

    investigate Math functions

---
+++ MathFunctionsSolution/MathFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Investigate Built-in Math Functions\n");
+
+double aNumber = 49;
+double result;
+
+//find the square root of a number
+result = Math.Sqrt(aNumber);
+Console.WriteLine($"the square root of {aNumber} is {result}\n");
+Console.WriteLine($"the value of {aNumber} * PI is {aNumber * Math.PI}\n");
