de353e52d457d2f348e2ce9dc669efb20155be1c
Don Welch <dwelch@nait.ca>
2023-02-14 13:50:43 -0700

    Assignment 2 HInts

---
+++ AssignmentHIntsSolution/AssignmentHInts/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//TestClearingConsole();
+TestStringPadLeft();
+
+
+
+void TestClearingConsole()
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Hello, World!");
+    Console.ReadLine();
+    Console.Clear();
+    Console.WriteLine("This should be the top line");
+    Console.ReadLine();
+}
+
+void TestStringPadLeft()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 0; j < 10; j++)
+
+        {
+            Console.Write($"{j}");
+        }
+    }
+    Console.WriteLine("");
+    string myString = "thisisalotofcharacters";
+    for (int i = 0; i <= 50; i = i + 5)
+    {
+        Console.WriteLine(myString.PadLeft(i,'*'));
+    }
+}
